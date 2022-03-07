7d74ac83c70d2bd5c91a3232798d4295e6350e8a
Caleeeb <daycinematography@gmail.com>
2022-03-07 11:39:27 -0500

    added fight function and console declaration

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,35 @@
 // create robot name
 var playerName = window.prompt("What is your robot's name?");
 
-console.log(playerName);
+//player robot stats
+var playerHealth = 100;
+var playerAttack = 10;
 
-// this creates a function named "fight"
-function fight() {
-    window.alert("The fight has begun!");
-}
+//enemy robot stats
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-//fight();
+//fight function
+var fight = function() {
+    //alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
 
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - enemyAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+};
+
+fight();
